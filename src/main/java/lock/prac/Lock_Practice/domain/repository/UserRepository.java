a2fdeed641d9506108859fdf2c47ea9303f0be92
parkmineum <a01087164428@gmail.com>
2025-05-26 20:11:51 +0900

    .gitignore 추가

--- src/main/java/lock/prac/Lock_Practice/domain/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package lock.prac.Lock_Practice.domain.repository;
-
-import lock.prac.Lock_Practice.domain.Entity.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
